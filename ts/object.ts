81ab41fe213e3187daa4e128f5ffdb46ff82284a
DESKTOP-N0FMIGE\猪猪 <422914219@qq.com>
2018-09-30 10:00:21 +0800

    临时提交

---
+++ ts/object.ts
@@ -0,0 +1,24 @@
+function f(x, y) {
+    return { x, y }
+}
+
+const o = {
+    method(){
+        return 'hello'
+    }
+}
+
+const cart = {
+    _wheels: 4,
+
+    get wheels() {
+        return this._wheels;
+    },
+
+    set wheels(value) {
+        if (value < this._wheels) {
+            throw new Error('数值太小了！');
+        }
+        this._wheels = value;
+    }
+}
